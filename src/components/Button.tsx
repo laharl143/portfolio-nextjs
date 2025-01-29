594cb59ee198842be80c92e39c5eaaa5db16f39c
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-01-29 14:53:43 +0800

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,23 @@
 import { ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 const Button = (
   props: {
     variant: "primary" | "secondary" | "text";
   } & ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-  const { className, children, ...rest } = props;
+  const { className, children, variant, ...rest } = props;
   return (
-    <button className={className} {...rest}>
+    <button
+      className={twMerge(
+        "",
+        variant === "primary" && "",
+        variant === "secondary" && "",
+        variant === "text" && "",
+        className
+      )}
+      {...rest}
+    >
       <span>{children}</span>
     </button>
   );
