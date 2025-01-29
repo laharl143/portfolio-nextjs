5e840f48b7e25b6c178f3747f9352d8990c0d6f2
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-01-29 16:41:13 +0800

    Stopped at 1:21:51

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import image1 from "@/assets/images/testimonial-1.jpg";
 import image2 from "@/assets/images/testimonial-2.jpg";
 import image3 from "@/assets/images/testimonial-3.jpg";
+import Image from "next/image.js";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const testimonials = [
@@ -35,7 +36,66 @@ const testimonials = [
 ];
 
 const Testimonials: FC = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section>
+      <h2>
+        <span>Some nice words from my past clients</span>
+        <span>Some nice words from my past clients</span>
+      </h2>
+      <div className="container">
+        <div>
+          {testimonials.map(({ name, company, role, quote, image }) => (
+            <div key={name}>
+              <div>
+                <Image src={image} alt={name} />
+              </div>
+              <blockquote>
+                <div>{quote}</div>
+                <cite>
+                  {name}, {role} at {company}
+                </cite>
+              </blockquote>
+            </div>
+          ))}
+        </div>
+        <div>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </button>
+          <button>
+            {/* Stopped at 1:21:51 https://www.youtube.com/watch?v=7ItfH0-4Xyc */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
