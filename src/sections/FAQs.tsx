417a0249f4be950fcd0459fba13d52765c42760e
Erskine Gamaliel L. Duenas <erskine.duenas@gmail.com>
2025-01-30 14:53:22 +0800

    Fixed: FAQs

--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -30,13 +30,15 @@ const FAQs: FC = () => {
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl">FAQs</h2>
         <div className="mt-10 md:mt-16 lg:mt-20">
-          {faqs.map(({ question, answer }) => (
+          {faqs.map(({ question }) => (
             <div
               key={question}
               className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py-10 last:border-b"
             >
               <div className="flex items-center justify-between gap-4">
-                <div className="text-2xl md:text-3xl lg:text-4xl">{question}</div>
+                <div className="text-2xl md:text-3xl lg:text-4xl">
+                  {question}
+                </div>
                 <div className="inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -27,35 +27,49 @@ const navItems = [
 
 const Header: FC = () => {
   return (
-    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
-      <div className="container !max-w-full">
-        <div className="flex justify-between h-20 items-center">
-          <div>
-            <a href="/">
-              <span className="text-xl font-bold uppercase">
-                Alex&nbsp; Taylor
-              </span>
-            </a>
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
-              </svg>
+    <header className="">
+      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md">
+        <div className="container !max-w-full">
+          <div className="flex justify-between h-20 items-center">
+            <div>
+              <a href="/">
+                <span className="text-xl font-bold uppercase text-white">
+                  Alex&nbsp; Taylor
+                </span>
+              </a>
             </div>
-            <Button
-              variant="primary"
-              className="hidden md:inline-flex items-center"
-            >
-              Contact Me
-            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="fixed top-0 left-0 w-full">
+        <div className="container !max-w-full">
+          <div className="flex justify-end h-20 items-center">
+            <div className="flex items-center gap-4">
+              <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="3"
+                    y="15"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                  />
+                  <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+                </svg>
+              </div>
+              <Button
+                variant="primary"
+                className="hidden md:inline-flex items-center"
+              >
+                Contact Me
+              </Button>
+            </div>
           </div>
         </div>
       </div>
