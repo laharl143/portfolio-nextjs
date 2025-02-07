39c01fedee398dd82dc05feb792f264bb2e729a6
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-02-07 13:42:01 +0800

    Fix: comments

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -11,14 +11,9 @@ export const skillsData = [
   'AWS',
   'Docker',
   'Vue',
-  // 'Firebase',
-  // 'MaterialUI',
-  // 'Nginx',
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
