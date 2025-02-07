59fc1456d62d8e47dc47780a0dad2ac5c82943cd
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-02-07 12:57:24 +0800

    Fix: skills section

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -8,29 +8,16 @@ import Marquee from "react-fast-marquee";
 
 function Skills() {
   return (
-    <div
+    <section
       id="skills"
-      className="relative"
+      className="section"
     >
-        <h2 className="text-4xl md:text-7xl lg:text-8xl container mt-20">Skills</h2>
-      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
-
+        <h2 className="text-4xl md:text-7xl lg:text-8xl container">Skills</h2>
       <div className="flex justify-center -translate-y-[1px]">
         <div className="w-3/4">
           <div className="h-[1px] bg-gradient-to-r from-transparent to-transparent w-full" />
         </div>
       </div>
-
-      {/* <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
-            Skills
-          </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-        </div>
-      </div> */}
-
       <div className="w-full my-12">
         <Marquee
           gradient={false}
@@ -46,7 +33,7 @@ function Skills() {
               className="w-36 min-w-fit h-fit flex flex-col items-center justify-center transition-all duration-500 m-3 sm:m-5 rounded-lg group relative hover:scale-[1.15] cursor-pointer"
               key={id}
             >
-              <div className="h-full w-full rounded-lg border border-black bg-transparent shadow-none shadow-gray-50 group-hover:border-neon-500 transition-all duration-500">
+              <div className="h-full w-full rounded-lg border border-gray-400 bg-transparent shadow-none shadow-gray-50 group-hover:border-neon-500 transition-all duration-500">
                 <div className="flex -translate-y-[1px] justify-center">
                   <div className="w-3/4">
                     <div className="h-[1px] w-full bg-gradient-to-r from-transparent border-neon-500 to-transparent" />
@@ -69,7 +56,7 @@ function Skills() {
           ))}
         </Marquee>
       </div>
-    </div>
+    </section>
   );
 }
 
