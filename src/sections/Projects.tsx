0b8693f0ff122ee81dc7ff8183f3c533edd62d03
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-02-13 09:17:36 +0800

    Add: in progress flag
    -fix: z props of tech icons

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -22,10 +22,11 @@ import { GiAtom } from "react-icons/gi";
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const projects = [
   {
-    name: "Airbnb Clone (in progress)",
+    name: "Airbnb Clone",
     tech: ["React", "TypeScript", "NextJs", "TailwindCSS", "MongoDBAtlas"],
     image: airbnb,
     href: "https://airbnb-clone-ers-due.vercel.app/",
+    inProgress: true,
   },
   {
     name: "Simon game",
@@ -111,104 +112,111 @@ const Projects: FC = () => {
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl">Projects</h2>
         <div className="mt-10 md:mt-16 lg:mt-20">
-          {projects.map(({ name, image, href, tech, comingSoon }) => (
-            <a
-              href={href}
-              key={name}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="border-t last:border-b border-stone-400 border-dotted py-6 md:py-8 lg:py-10 flex flex-col relative group/project"
-            >
-              {/* Background hover effect */}
-              <div className="absolute bottom-0 left-0 w-full h-0 group-hover/project:h-full transition-all duration-700 bg-stone-300 dark:bg-stone-700"></div>
-              <div className="relative">
-                {/* Mobile Image */}
-                <div className="aspect-video md:hidden">
-                  <Image
-                    src={image}
-                    alt={`${name} image`}
-                    className="size-full object-cover"
-                  />
-                </div>
-                <div className="mt-8 md:mt-0 flex justify-between items-center md:grid md:[grid-template-columns:1fr_300px_max-content] md:gap-8">
-                  {/* Project Name */}
-                  <div className="lg:group-hover/project:pl-8 transition-all duration-700">
-                    <h3 className="text-2xl md:text-3xl lg:text-4xl text-gray-900 dark:text-white">
-                      {name}
-                    </h3>
-                    {/* Tech Stack (Hidden by default, appears on hover) */}
-                    <div className="opacity-0 group-hover/project:opacity-100 transition-opacity duration-500 mt-2">
-                      <div className="flex gap-3 opacity-0 group-hover/project:opacity-100 transition-opacity duration-500 mt-2">
-                        {tech.map((t) => (
-                          <div key={t} className="group relative">
-                            {techIcons[t] || t}
-                            {/* Tech Stack text with dynamic background color */}
-                            <div
-                              className={`absolute opacity-0 group-hover:opacity-100 left-1/2 -translate-x-1/2 top-8 text-white text-xs p-2 rounded-lg transition-opacity duration-300 ${techColors[t]}`}
-                            >
-                              {t}
+          {projects.map(
+            ({ name, image, href, tech, comingSoon, inProgress }) => (
+              <a
+                href={href}
+                key={name}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-t last:border-b border-stone-400 border-dotted py-6 md:py-8 lg:py-10 flex flex-col relative group/project"
+              >
+                {/* Background hover effect */}
+                <div className="absolute bottom-0 left-0 w-full h-0 group-hover/project:h-full transition-all duration-700 bg-stone-300 dark:bg-stone-700"></div>
+                <div className="relative">
+                  {/* Mobile Image */}
+                  <div className="aspect-video md:hidden">
+                    <Image
+                      src={image}
+                      alt={`${name} image`}
+                      className="size-full object-cover"
+                    />
+                  </div>
+                  <div className="mt-8 md:mt-0 flex justify-between items-center md:grid md:[grid-template-columns:1fr_300px_max-content] md:gap-8">
+                    {/* Project Name */}
+                    <div className="lg:group-hover/project:pl-8 transition-all duration-700">
+                      <h3 className="text-2xl md:text-3xl lg:text-4xl text-gray-900 dark:text-white">
+                        {name}
+                      </h3>
+                      {/* Tech Stack (Hidden by default, appears on hover) */}
+                      <div className="opacity-0 group-hover/project:opacity-100 transition-opacity duration-500 mt-2">
+                        <div className="flex gap-3 opacity-0 group-hover/project:opacity-100 transition-opacity duration-500 mt-2">
+                          {tech.map((t) => (
+                            <div key={t} className="group relative z-30">
+                              {techIcons[t] || t}
+                              {/* Tech Stack text with dynamic background color */}
+                              <div
+                                className={`absolute opacity-0 group-hover:opacity-100 left-1/2 -translate-x-1/2 top-8 text-white text-xs p-2 rounded-lg transition-opacity duration-300 ${techColors[t]}`}
+                              >
+                                {t}
+                              </div>
                             </div>
-                          </div>
-                        ))}
+                          ))}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  {/* Coming Soon Banner */}
-                  {comingSoon && (
-                    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 text-white text-lg font-semibold rounded-md z-20 opacity-0 group-hover/project:opacity-100 transition-opacity duration-300">
-                      <span className="animate-pulse">Coming Soon!</span>
+                    {/* Coming Soon Banner */}
+                    {comingSoon && (
+                      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 text-white text-lg font-semibold rounded-md z-20 opacity-0 group-hover/project:opacity-100 transition-opacity duration-300">
+                        <span className="animate-pulse">Coming Soon!</span>
+                      </div>
+                    )}
+                    {/* In Progress Banner */}
+                    {inProgress && (
+                      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-yellow-600 bg-opacity-50 text-white text-lg font-semibold rounded-md z-20 opacity-0 group-hover/project:opacity-100 transition-opacity duration-300">
+                        <span className="animate-pulse">In Progress!</span>
+                      </div>
+                    )}
+                    {/* Hover Image */}
+                    <div className="relative">
+                      <div className="absolute aspect-video w-full top-1/2-translate-y-1/2 opacity-0 scale-90 group-hover/project:opacity-100 group-hover/project:scale-100 lg:group-hover/project:scale-110 transition-all duration-500 z-10">
+                        <Image
+                          src={image}
+                          alt={`${name} image`}
+                          className="size-full object-cover"
+                        />
+                      </div>
                     </div>
-                  )}
-
-                  {/* Hover Image */}
-                  <div className="relative">
-                    <div className="absolute aspect-video w-full top-1/2-translate-y-1/2 opacity-0 scale-90 group-hover/project:opacity-100 group-hover/project:scale-100 lg:group-hover/project:scale-110 transition-all duration-500 z-10">
-                      <Image
-                        src={image}
-                        alt={`${name} image`}
-                        className="size-full object-cover"
-                      />
-                    </div>
-                  </div>
-                  {/* Arrow Animation */}
-                  <div className="lg:group-hover/project:pr-8 transition-all duration-700">
-                    <div className="size-6 overflow-hidden">
-                      <div className="h-6 w-12 flex group-hover/project:-translate-x-1/2 transition-transform duration-500">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                          />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                          />
-                        </svg>
+                    {/* Arrow Animation */}
+                    <div className="lg:group-hover/project:pr-8 transition-all duration-700">
+                      <div className="size-6 overflow-hidden">
+                        <div className="h-6 w-12 flex group-hover/project:-translate-x-1/2 transition-transform duration-500">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                            />
+                          </svg>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                            />
+                          </svg>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </a>
-          ))}
+              </a>
+            )
+          )}
         </div>
       </div>
     </section>
